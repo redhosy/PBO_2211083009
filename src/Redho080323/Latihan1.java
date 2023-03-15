1fbbcb1eae5bb83be2688b936c3e647ebae0655c
redhosy <127164173+redhosy@users.noreply.github.com>
2023-03-15 22:52:53 +0700

    Latihan

---
+++ src/Redho080323/Latihan1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Redho080323;
+
+/**
+ *
+ * @author septa
+ */
+
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+
+public class Latihan1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.print("Enter word1: ");
+        String word1 = reader.readLine();
+        
+        System.out.print("Enter word2: ");
+        String word2 = reader.readLine();
+        
+        System.out.print("Enter word3: ");
+        String word3 = reader.readLine();
+        
+        System.out.println(word1 + " " + word2 + " " + word3);
+    }
+}
+
+
---
+++ src/Redho080323/Latihan2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Redho080323;
+
+/**
+ *
+ * @author septa
+ */
+import javax.swing.JOptionPane; 
+public class Latihan2 {  
+     public static void main( String[] args ){ 
+     String word1 = ""; 
+     String word2 = "";
+     String word3 = "";
+      word1 = JOptionPane.showInputDialog("Enter word1:");
+      word2 = JOptionPane.showInputDialog("Enter word2:");
+      word3 = JOptionPane.showInputDialog("Enter word3:");
+        
+     String msg = word1 + " " + word2 + " " + word3;
+        
+ 
+ JOptionPane.showMessageDialog(null, msg); 
+ 
+ } 
+} 
+
